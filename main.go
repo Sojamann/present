c3ab420b969362e820e3d955458896bf8e12883d
Robin Bergewski <robinbergewski@gmail.com>
2023-10-22 14:13:04 +0200

    Add: most basic tui

--- main.go
+++ main.go
@@ -1,40 +1,90 @@
 package main
 
 import (
-	"bufio"
+	"fmt"
+	"log"
 	"os"
 	"strings"
+
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-func splitAt(sep string) bufio.SplitFunc {
-	return func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-		 // Return nothing if at end of file and no data passed
-        if atEOF && len(data) == 0 {
-            return 0, nil, nil
-        }
+type model struct {
+	slides []string
 
-        // Find the index of the input of the separator sep
-        if i := strings.Index(string(data), sep); i >= 0 {
-            return i + len(sep), data[0:i], nil
-        }
+	currSlide int
 
-        // If at end of file with data return the data
-        if atEOF {
-            return len(data), data, nil
-        }
-
-		return 0, nil, nil
-	}
+	ready bool
+	vp viewport.Model
 }
+
+func (m *model) Init() tea.Cmd {
+	return nil
+}
+
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		cmd  tea.Cmd
+		cmds []tea.Cmd
+	)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		key := msg.String()
+		switch key {
+		case "q":
+			return m, tea.Quit
+		case "l", tea.KeyRight.String():
+			m.currSlide = min(m.currSlide+1, len(m.slides)-1)
+			m.vp.SetContent(m.slides[m.currSlide])
+		case "h", tea.KeyLeft.String():
+			m.currSlide = max(0, m.currSlide-1)
+			m.vp.SetContent(m.slides[m.currSlide])
+		}
+
+	case tea.WindowSizeMsg:
+		// -1 because of footer
+		if !m.ready { 
+			m.vp = viewport.New(msg.Width, msg.Height-1)
+			m.vp.SetContent(m.slides[m.currSlide])
+			m.ready = true
+		} else {
+			m.vp.Width = msg.Width
+			m.vp.Height = msg.Height-1
+		}
+	}
+
+	// THe viewport might wants to do some things
+	m.vp, cmd = m.vp.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m *model) View() string {
+	if !m.ready {
+		return "\n Loading ... "
+	}
+
+	footer := lipgloss.NewStyle().Render("q - Quit | h/Left - prev slide | l/Right - next slide")
+
+	return fmt.Sprintf("%s\n%s", m.vp.View(), footer)
+}
+
 
 func main() {
-	file, _ := os.Open(os.Args[1])
-	scanner := bufio.NewScanner(file)
-	scanner.Split(splitAt("---"))
+	data, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	for scanner.Scan() {
-		text := strings.TrimSpace(scanner.Text())
-		println("Slide: ", text)
+	m := &model {
+		slides: strings.Split(string(data), "---"),
+	}
+	p := tea.NewProgram(m, tea.WithAltScreen());
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
 	}
 }
-
