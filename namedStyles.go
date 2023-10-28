77f3aed377f7794b2713467e0673e4e734c08ade
Robin Bergewski <robinbergewski@gmail.com>
2023-10-28 16:35:35 +0200

    Add: named styles for common colors

--- namedStyles.go
+++ namedStyles.go
@@ -6,9 +6,16 @@ import (
 
 // renders normal text
 var namedStyleLookupTable = map[string]lipgloss.Style{
-	"h1": lipgloss.NewStyle().
-		Bold(true).
-		Background(lipgloss.Color("#F25D94")),
+	"h": lipgloss.NewStyle().
+			Bold(true).
+			Background(lipgloss.Color("#F25D94")),
 	"b": lipgloss.NewStyle().Bold(true),
 	"i": lipgloss.NewStyle().Italic(true),
+
+	// colors
+	"red": lipgloss.NewStyle().Foreground(lipgloss.Color("1")),
+	"green": lipgloss.NewStyle().Foreground(lipgloss.Color("2")),
+	"yellow": lipgloss.NewStyle().Foreground(lipgloss.Color("3")),
+	"blue": lipgloss.NewStyle().Foreground(lipgloss.Color("4")),
+	"white": lipgloss.NewStyle().Foreground(lipgloss.Color("7")),
 }
